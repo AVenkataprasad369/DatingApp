046b8427a4991ef460be35eda27f3e12b9d5ee76
AVenkataprasad369 <Adireddi.venkataprasad@gmail.com>
2021-12-17 14:17:01 +0530

    End of Section14 - Adding the Likes feature

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -11,5 +11,30 @@ namespace API.Data
        }
 
        public DbSet<AppUser> Users { get; set; }
+       public DbSet<UserLike> Likes { get; set; }
+
+       protected override void OnModelCreating(ModelBuilder builder)
+       {
+           base.OnModelCreating(builder);
+
+           builder.Entity<UserLike>()
+           .HasKey(k => new {k.SourceUserId, k.LikedUserId});
+
+           //Important: If you are using SQL Server then you need to set the DeleteBehaviour here 
+           //to DeleteBehaviour.NoAction Or you will get an error during migration
+           builder.Entity<UserLike>()
+           .HasOne(s => s.SourceUser)
+           .WithMany(l => l.LikedUsers)
+           .HasForeignKey(s => s.SourceUserId)
+           .OnDelete(DeleteBehavior.Cascade); 
+
+           //Important: If you are using SQL Server then you need to set the DeleteBehaviour here 
+           //to DeleteBehaviour.NoAction Or you will get an error during migration
+           builder.Entity<UserLike>()
+           .HasOne(s => s.LikedUser)
+           .WithMany(l => l.LikedByUsers)
+           .HasForeignKey(s => s.LikedUserId)
+           .OnDelete(DeleteBehavior.Cascade);
+       }
     }
 }
